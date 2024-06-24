df858ebcb79db6b64254e98789d0db865e1809af
Alexandre Tabouret <Alexandre.Tabouret@bordeaux-inp.fr>
2024-06-24 16:47:54 +0900

    Added a few commentray regarding constants

--- src/constants.py
+++ src/constants.py
@@ -2,8 +2,9 @@ from enum import Enum
 from widgets.modelFrames.decisionTreeFrame import DecisionTreeFrame
 from widgets.modelFrames.randomForestFrame import RandomForestFrame
 
+# Dictionnary associating models names to corresponding frame class (customtkinter)
 MODEL_FRAMES = {"Decision tree": DecisionTreeFrame, "Random forest": RandomForestFrame}
 
 class Models(Enum):
-    CLASSIFIERS = ["Decision tree", "Random forest"]
-    GRADERS = ["Decision tree", "Random forest"]
+    CLASSIFIERS = ["Decision tree", "Random forest"] # List of available classifier
+    GRADERS = ["Decision tree", "Random forest"] # List of available grader
