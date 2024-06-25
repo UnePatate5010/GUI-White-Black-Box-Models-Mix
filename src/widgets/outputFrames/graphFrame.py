8ec942dccdd1cccadb53797f16a9fc5ad66cfd21
UnePatate5010 <tabouret.alexandre@gmail.com>
2024-06-25 11:22:56 +0900

    fixed canvas size too

--- src/widgets/outputFrames/graphFrame.py
+++ src/widgets/outputFrames/graphFrame.py
@@ -5,6 +5,8 @@ class GraphFrame(ctk.CTkFrame):
         super().__init__(master)
 
         self.grid_propagate(False) # Prevent the frame from changing size depending on widgets inside
+        self.grid_columnconfigure(0, weight=1)
+        self.grid_rowconfigure(0, weight=1)
 
 
         self.canvas = ctk.CTkCanvas(self)
