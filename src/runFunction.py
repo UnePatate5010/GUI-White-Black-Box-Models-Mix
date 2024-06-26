c804967278605b8e24b1b2170f176f97870bb669
UnePatate5010 <tabouret.alexandre@gmail.com>
2024-06-26 14:25:29 +0900

    created run experiment function file

---
+++ src/runFunction.py
@@ -0,0 +1,6 @@
+def run():
+        # Get methods and hyperparameters
+        # Format them correctly (convert dicts to reflect real arguments names)
+        # Call the main function
+        # Update output frames with results
+    pass
