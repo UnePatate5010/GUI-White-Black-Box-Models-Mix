05ade73ee09bbb19119bb3bd1d1476fa25c28538
UnePatate5010 <tabouret.alexandre@gmail.com>
2024-07-16 18:02:46 +0900

    raised the number of points

--- src/widgets/outputFrames/graphFrame.py
+++ src/widgets/outputFrames/graphFrame.py
@@ -78,8 +78,8 @@ class GraphFrame(ctk.CTkFrame):
         self.fig.append(Figure(figsize = (10, 5), dpi = 100, facecolor="grey"))
         plot = self.fig[-1].add_subplot(111)
 
-        xx, yy = np.meshgrid(np.arange(X[:, 0].min() -0.1, X[:, 0].max() + 0.1, (X[:, 1].max() - X[:, 1].min()) / 300),
-                     np.arange(X[:, 1].min() - 0.1, X[:, 1].max() + 0.1, (X[:, 1].max() - X[:, 1].min()) / 300))
+        xx, yy = np.meshgrid(np.arange(X[:, 0].min() -0.1, X[:, 0].max() + 0.1, (X[:, 1].max() - X[:, 1].min()) / 500),
+                     np.arange(X[:, 1].min() - 0.1, X[:, 1].max() + 0.1, (X[:, 1].max() - X[:, 1].min()) / 500))
         
         # List of unique labels
         y_u = np.unique(y)
